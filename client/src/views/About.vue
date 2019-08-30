6451677a70c91e940702473db95910b484fbdd0f
Nico <nicbritos@hotmail.com>
2019-08-29 22:24:28 -0300

    Changed h1 to card-title

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,16 +1,18 @@
 <template
   ><v-container grid-list-md>
     <v-card>
-      <h1 class="ml-5">About</h1>
-      <v-card-text class="ml-1">
+      <v-card-title>
+        About
+      </v-card-title>
+      <v-card-text>
         This website allows managing data from surveys made on workshops about
         Sensitization about Climate Change.
       </v-card-text>
-      <v-card-text class="ml-1"></v-card-text>
-      <v-card-text class="ml-1">
+      <v-card-text></v-card-text>
+      <v-card-text>
         Current version: 0.1.0b
       </v-card-text>
-      <v-card-text class="ml-1">
+      <v-card-text>
         Created by: Nicolas Ignacio Britos. Contact at:
         <a href="mailto:nicbritos@hotmail.com"> nicbritos@hotmail.com</a> or
         <a href="mailto:nbritos@itba.edu.ar">
