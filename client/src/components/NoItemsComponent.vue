0522b1b7798edba5d06ee6d210aa40808949d5f4
Nico <nicbritos@hotmail.com>
2019-09-12 22:01:44 -0300

    Added component to display a message when no item is found

---
+++ client/src/components/NoItemsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <h4 class="no-bold" v-if="!search">Add a new {{ type }}</h4>
+  <h4 class="no-bold" v-else>No results found</h4>
+</template>
+
+<script>
+export default {
+  name: "NoItemsComponent",
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    search: {
+      type: Boolean,
+      required: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.no-bold {
+  font-weight: normal;
+}
+</style>
